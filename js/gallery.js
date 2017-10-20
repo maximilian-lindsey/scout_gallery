8428df93bf9bcfbf6e90bfc78dd1d43766d67a49
Maximilian Ruppert <tools@loveforpixels.com>
2017-10-20 15:47:25 +0200

    add: click event handler draft, first image config option

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,26 @@
 class Gallery {
-	constructor(galleryContainerId = '', imageURLs = []) {
+	constructor(galleryContainerId = '', imageURLs = [], firstImage = 0) {
 		if(galleryContainerId == ''){
 			throw Error('galleryContainerId is missing');
 		}
 		if(imageURLs.length == 0){
 			throw Error('no imageURLs recieved');
 		}
+		if(firstImage >= imageURLs.length){
+			throw Error('firstImage not found in imageURLs');
+		}		
 		else{
 			this.galleryContainerId = galleryContainerId;
 			this.imageURLs = imageURLs;
+			this.currentImage = (firstImage >= 0) ? firstImage : 0;
 			this.createDOMElements();
 		}
 	}
 
 	createDOMElements(){
 		const galleryContainer = document.getElementById(this.galleryContainerId);
-		try {			
+		try {
+			// create nodes
 			let galleryRoot = document.createElement('div');			
 			let galleryImageWrapper = document.createElement('div');
 			let galleryImage = document.createElement('img');
@@ -23,7 +28,7 @@ class Gallery {
 			let galleryPrevBtn = document.createElement('button');
 			let galleryPaginationLabel = document.createElement('span');
 			let galleryNextBtn = document.createElement('button');
-
+			// add classes
 			galleryRoot.className = 'gallery';
 			galleryImageWrapper.className = 'gallery-images';
 			galleryImage.className = 'gallery-images-image';
@@ -31,11 +36,19 @@ class Gallery {
 			galleryPrevBtn.className = 'gallery-controlls-button';			
 			galleryPaginationLabel.className = 'gallery-controlls-pagination';			
 			galleryNextBtn.className = 'gallery-controlls-button';
-
+			// add content
+			galleryImage.src = this.imageURLs[this.currentImage];
 			galleryPrevBtn.textContent = '←';
-			galleryPaginationLabel.textContent = `1/${this.imageURLs.length}`;
+			galleryPaginationLabel.textContent = `${this.currentImage}/${this.imageURLs.length}`;
 			galleryNextBtn.textContent = '→';
-
+			// add event listeners
+			galleryPrevBtn.addEventListener('click', (event)=>{
+				this.getPreviousImage(event);
+			});
+			galleryNextBtn.addEventListener('click', (event)=>{
+				this.getNextImage(event);
+			});
+			// append nodes to dom
 			galleryImageWrapper.appendChild(galleryImage);
 			galleryControllsWrapper.appendChild(galleryPrevBtn);
 			galleryControllsWrapper.appendChild(galleryPaginationLabel);
@@ -47,13 +60,18 @@ class Gallery {
 			if(!galleryContainer){
 				throw Error(`galleryContainer with id ${this.galleryContainerId} was not found`);
 			}
-		}
-		
+		}		
 	}
-	
+
+	getPreviousImage(event){
+		console.log(event);
+	}
+	getNextImage(event){
+		console.log(event);
+	}
 }
 
 
 window.onload = ()=>{
-	const gallery = new Gallery('root', ['https://placebear.com/g/500/500', 'https://placebear.com/g/500/501', 'https://placebear.com/g/500/502', 'https://placebear.com/g/500/503']);	
+	const gallery = new Gallery('root', ['https://placebear.com/g/500/500', 'https://placebear.com/g/500/501', 'https://placebear.com/g/500/502', 'https://placebear.com/g/500/503'], 0);	
 }
